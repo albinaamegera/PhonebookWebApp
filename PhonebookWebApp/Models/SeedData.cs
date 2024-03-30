c625832b70d670ce154858f76e8b8a008798352c
albinaamegera <36966826+albinaamegera@users.noreply.github.com>
2024-03-30 20:59:29 +0300

    two view pages released

---
+++ PhonebookWebApp/Models/SeedData.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PhonebookWebApp.Data;
+
+namespace PhonebookWebApp.Models
+{
+	public class SeedData
+	{
+		public static void Initialize(IServiceProvider serviceProvider)
+		{
+			using (PhonebookWebAppContext context = new(
+				serviceProvider.GetRequiredService<
+					DbContextOptions<PhonebookWebAppContext>>()))
+			{
+				if (context.Note.Any())
+				{
+					return;
+				}
+				context.Note.AddRange(
+					new Note
+					{
+						SecondName = "SName1",
+						Name = "Name1",
+						ThirdName = "THName1",
+						Phone = "+1 111 111 11 11",
+						Address = "Russian Federation",
+						Description = "bad guy"
+					},
+					new Note
+					{
+						SecondName = "SName2",
+						Name = "Name2",
+						ThirdName = "THName2",
+						Phone = "+2 222 222 22 22",
+						Address = "Russian Federation",
+						Description = "good guy"
+					},
+					new Note
+					{
+						SecondName = "SName3",
+						Name = "Name3",
+						ThirdName = "THName3",
+						Phone = "+3 333 333 33 33",
+						Address = "Russian Federation",
+						Description = "so so guy"
+					},
+					new Note
+					{
+						SecondName = "SName4",
+						Name = "Name4",
+						ThirdName = "THName4",
+						Phone = "+4 444 444 44 44",
+						Address = "Russian Federation",
+						Description = "bestie"
+					}
+				);
+				context.SaveChanges();
+			}
+		}
+	}
+}
--- PhonebookWebApp/Program.cs
+++ PhonebookWebApp/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using PhonebookWebApp.Data;
+using PhonebookWebApp.Models;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<PhonebookWebAppContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("PhonebookWebAppContext") ?? throw new InvalidOperationException("Connection string 'PhonebookWebAppContext' not found.")));
@@ -9,6 +10,12 @@ builder.Services.AddDbContext<PhonebookWebAppContext>(options =>
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+	var services = scope.ServiceProvider;
+	SeedData.Initialize(services);
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -27,6 +34,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
 	name: "default",
-	pattern: "{controller=Home}/{action=Index}/{id?}");
+	pattern: "{controller=Notes}/{action=Index}/{id?}");
 
 app.Run();
